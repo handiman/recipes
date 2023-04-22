9e9233490e0bbcfbabb705b9399760927b1115c8
Henrik Becker <handiman@users.noreply.github.com>
2023-04-22 12:34:48 +0200

    Merge madness

--- _plugins/UpdateRecipes.rb
+++ _plugins/UpdateRecipes.rb
@@ -7,7 +7,6 @@ module Jekyll
         begin
           File.open("./_recipes/#{File.basename(jsonfilename, '.json')}.md", "w") do | markdown |
             json = JSON.load_file(jsonfilename) 
-<<<<<<< HEAD
             @step = ""
             markdown.puts "---"
             markdown.puts "title: #{(json['name'] || 'Namnlös').strip}"
@@ -64,48 +63,14 @@ module Jekyll
             if json['url']
               @step = "Render URL"
               url = json['url'].strip
-=======
-            title = json['name'].strip
-            yields = json['recipeYield'].strip
-            totaltime = "#{Integer(Time.parse(json['totalTime'].strip).strftime('%I'))} tim #{Time.parse(json['totalTime'].strip).strftime('%M')} min"
-            description = json['description'].strip
-            instructions = json['recipeInstructions'].strip
-            url = json['url'].strip
-            ingredients = json['recipeIngredient'].select { | ingredient | ingredient.strip }
-
-            markdown.puts "---"
-            markdown.puts "title: #{title}"
-            markdown.puts "---"
-            markdown.puts "# #{title}"
-            markdown.puts
-            markdown.puts description
-            markdown.puts 
-            markdown.puts "| #{'Antal portioner'.ljust(['Antal portioner'.length, yields.length].max, ' ')} | #{'Total tid'.ljust(['Total tid'.length, totaltime.length].max, ' ')} |"
-            markdown.puts "| #{''.ljust(['Antal portioner'.length, yields.length].max, '-')} | #{''.ljust(['Total tid'.length, totaltime.length].max, '-')} |"
-            markdown.puts "| #{yields.ljust(['Antal portioner'.length, yields.length].max, ' ')} | #{totaltime.ljust(['Total tid'.length, totaltime.length].max, ' ')} |"
-            markdown.puts
-            markdown.puts "## Ingredienser"
-            ingredients.each do | ingredient |
-              markdown.puts "* #{ingredient}"
-            end
-            markdown.puts
-            markdown.puts "# Tillagning"
-            markdown.puts instructions
-            if url
->>>>>>> b8ca9fdfbeae27eb4cc8d4ddcec34ccaf371a831
               markdown.puts
               markdown.puts "Recept från [#{url}](#{url})."
             end 
             markdown.close()
           end
-<<<<<<< HEAD
         rescue => e
           puts "---#{File.basename(jsonfilename, '.json')}---"
           print "#{e.class} at '#{@step}' in #{jsonfilename}\n#{e.message}"
-=======
-        rescue
-          print "Error parsing #{jsonfilename}"
->>>>>>> b8ca9fdfbeae27eb4cc8d4ddcec34ccaf371a831
         end
       end
     end  
